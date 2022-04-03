d2ca934b8403f77632a5359745bacc75575ce733
Andrey <dorst.andreas@gmail.com>
2022-04-03 18:04:15 +0300

    feature homework 8

--- project/client.py
+++ project/client.py
@@ -6,11 +6,13 @@ import socket
 import time
 import argparse
 import logging
+import threading
 import logs.config_client_log
 from common.variables import DEFAULT_PORT, DEFAULT_IP_ADDRESS, \
-    ACTION, TIME, USER, ACCOUNT_NAME, SENDER, PRESENCE, RESPONSE, ERROR, MESSAGE, MESSAGE_TEXT
+    ACTION, TIME, USER, ACCOUNT_NAME, SENDER, PRESENCE, RESPONSE, \
+    ERROR, MESSAGE, MESSAGE_TEXT, DESTINATION, EXIT
 from common.utils import get_message, send_message
-from errors import ReqFieldMissingError, ServerError
+from errors import IncorrectDataReceivedError, ReqFieldMissingError, ServerError
 from decos import Log
 
 # Initial client logger
@@ -18,16 +20,44 @@ CLIENT_LOGGER = logging.getLogger('client')
 
 
 @Log(CLIENT_LOGGER)
-def message_from_server(message):
+def create_exit_message(account_name):
     """
-    The function generates message from server
-    :param message:
+    The function create dictionary with entry message
+    :param account_name:
+    :return:
     """
-    if ACTION in message and message[ACTION] == MESSAGE and SENDER in message and MESSAGE_TEXT in message:
-        print(f'Received message from user {message[SENDER]}:\n{message[MESSAGE_TEXT]}')
-        CLIENT_LOGGER.info(f'Received message from user {message[SENDER]}:\n{message[MESSAGE_TEXT]}')
-    else:
-        CLIENT_LOGGER.error(f'Received bad message from server: {message}')
+    return {
+        ACTION: EXIT,
+        TIME: time.time(),
+        ACCOUNT_NAME: account_name
+    }
+
+
+@Log(CLIENT_LOGGER)
+def message_from_server(sock, my_username):
+    """
+    The handlers function for messages from server
+    :param sock:
+    :param my_username:
+    """
+    while True:
+        try:
+            message = get_message(sock)
+            if ACTION in message and message[ACTION] == MESSAGE and \
+                    SENDER in message and DESTINATION in message \
+                    and MESSAGE_TEXT in message and MESSAGE[DESTINATION] == my_username:
+                print(f'\nReceived message from user {message[SENDER]}:'
+                      f'\n{message[MESSAGE_TEXT]}')
+                CLIENT_LOGGER.info(f'Received message from user {message[SENDER]}:'
+                                   f'\n{message[MESSAGE_TEXT]}')
+            else:
+                CLIENT_LOGGER.error(f'Received incorrect server from server: {message}')
+        except IncorrectDataReceivedError:
+            CLIENT_LOGGER.error(f'Decoding message failed')
+        except (OSError, ConnectionError, ConnectionAbortedError,
+                ConnectionResetError, json.JSONDecodeError):
+            CLIENT_LOGGER.critical(f'Server connection lost')
+            break
 
 
 @Log(CLIENT_LOGGER)
@@ -36,30 +66,56 @@ def create_message(sock, account_name='Guest'):
     The function ask message text and return it
     :param sock:
     :param account_name:
-    :return message_dict:
     """
-    message = input(f'Please input message for send or !!! for exit: ')
-    if message == '!!!':
-        sock.close()
-        CLIENT_LOGGER.info('Finished by user')
-        print('Service closed')
-        sys.exit(0)
+    to_user = input('Input the recipient of the message: ')
+    message = input('Input the message for send')
     message_dict = {
         ACTION: MESSAGE,
+        SENDER: account_name,
+        DESTINATION: to_user,
         TIME: time.time(),
-        ACCOUNT_NAME: account_name,
         MESSAGE_TEXT: message
     }
-    CLIENT_LOGGER.debug(f'Message dict was formed: {message_dict}')
-    return message_dict
+    CLIENT_LOGGER.debug(f'Messages dictionary has formed: {message_dict}')
+    try:
+        send_message(sock, message_dict)
+        CLIENT_LOGGER.info(f'Message to user {to_user} has sent')
+    except Exception as e:
+        print(e)
+        CLIENT_LOGGER.critical('Server connection has lost')
+        sys.exit(1)
 
 
 @Log(CLIENT_LOGGER)
-def create_presence(account_name='Guest'):
+def user_interactive(sock, username):
+    """
+    The function for communication with user
+    :param sock:
+    :param username:
+    """
+    while True:
+        command = input('Input the command: ')
+        if command == 'message':
+            create_message(sock, username)
+        elif command == 'help':
+            print_help()
+        elif command == 'exit':
+            send_message(sock, create_exit_message(username))
+            print('Connection is closing...')
+            CLIENT_LOGGER.info('Connection is closing by user...')
+            # Delay need to send message about exit
+            time.sleep(0.5)
+            break
+        else:
+            print('Unknown command, please try again')
+
+
+@Log(CLIENT_LOGGER)
+def create_presence(account_name):
     """
     The function generates a client presence request
     :param account_name:
-    :return out:
+    :return:
     """
     out = {
         ACTION: PRESENCE,
@@ -72,11 +128,23 @@ def create_presence(account_name='Guest'):
     return out
 
 
+def print_help():
+    """
+    The function print help
+    """
+    print('Supported commands: ')
+    print('message - send message')
+    print('help - print hints for commands')
+    print('exit - exit from program')
+
+
 @Log(CLIENT_LOGGER)
 def process_response_ans(message):
     """
     The function parses the server response
+    return 200 if all is OK or generated exception
     :param message:
+    :return:
     """
     CLIENT_LOGGER.debug(f'Parse greeting message from server: {message}')
     if RESPONSE in message:
@@ -120,22 +188,32 @@ def arg_parser():
 
 def main():
     """
-    Loading command line options
+    The main function
     """
-    server_address, server_port, client_mode = arg_parser()
-    CLIENT_LOGGER.info(f'Started client with params: adr: {server_address} | prt: {server_port} | mod: {client_mode}')
+    # Loading command line options
+    server_address, server_port, client_name = arg_parser()
+
+    # Starting alert
+    print(f'Messanger. Client module. Client name: {client_name}')
+
+    # if not client name, ask for client name
+    if not client_name:
+        client_name = input('Input the client name: ')
+
+    CLIENT_LOGGER.info(f'Started client with params: adr: {server_address} | port: {server_port} | name: {client_name}')
     # Initial socket
     try:
         transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         transport.connect((server_address, server_port))
-        message_to_server = create_presence()
-        send_message(transport, message_to_server)
+        send_message(transport, create_message(client_name))
         answer = process_response_ans(get_message(transport))
         CLIENT_LOGGER.info(f'Connection done. Server response {answer}')
         print('Connection done')
-        print(answer)
     except json.JSONDecodeError:
-        CLIENT_LOGGER.error('Bad JSON data')
+        CLIENT_LOGGER.error('Decoding JSON string has failed')
+        sys.exit(1)
+    except ServerError as error:
+        CLIENT_LOGGER.error(f'Error connection to server: {error.text}')
         sys.exit(1)
     except ReqFieldMissingError as missing_error:
         CLIENT_LOGGER.error(f'Miss field {missing_error.missing_field}')
@@ -144,26 +222,21 @@ def main():
         CLIENT_LOGGER.critical(f'Server out {server_address}:{server_port}')
         sys.exit(1)
     else:
-        # If connection is correct
-        if client_mode == 'send':
-            print('Send messages mode')
-        else:
-            print('Receive messages mode')
+        # Start client process if connection is OK
+        receiver = threading.Thread(target=message_from_server, args=(transport, client_name), daemon=True)
+        receiver.start()
+
+        # Start sending messages
+        user_interface = threading.Thread(target=user_interactive, args=(transport, client_name), daemon=True)
+        user_interface.start()
+        CLIENT_LOGGER.debug('Processes has started')
+
+        # Watchdog is main loop, if one of threads has finished connection closed/lost
         while True:
-            # Send message mode
-            if client_mode == 'send':
-                try:
-                    send_message(transport, create_message(transport))
-                except (ConnectionError, ConnectionResetError, ConnectionRefusedError):
-                    CLIENT_LOGGER.error(f'Server connection {server_address} has lost')
-                    sys.exit(1)
-            # Receive message mode
-            if client_mode == 'listen':
-                try:
-                    message_from_server(get_message(transport))
-                except (ConnectionError, ConnectionResetError, ConnectionRefusedError):
-                    CLIENT_LOGGER.error(f'Server connection {server_address} has lost')
-                    sys.exit(1)
+            time.sleep(1)
+            if receiver.is_alive() and user_interface.is_alive():
+                continue
+            break
 
 
 if __name__ == '__main__':
--- project/server.py
+++ project/server.py
@@ -5,11 +5,11 @@ import sys
 import argparse
 import logging
 import select
-import time
 import logs.config_server_log
 from errors import IncorrectDataReceivedError
-from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, SENDER, MAX_CONNECTIONS, MESSAGE, MESSAGE_TEXT, \
-    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
+from common.variables import DEFAULT_PORT, MAX_CONNECTIONS, ACTION, TIME, \
+    USER, ACCOUNT_NAME, SENDER, PRESENCE, ERROR, MESSAGE, \
+    MESSAGE_TEXT, RESPONSE_400, DESTINATION, RESPONSE_200, EXIT
 from common.utils import get_message, send_message
 from decos import Log
 
@@ -18,7 +18,7 @@ SERVER_LOGGER = logging.getLogger('server')
 
 
 @Log(SERVER_LOGGER)
-def process_client_message(message, messages_list, client):
+def process_client_message(message, messages_list, client, clients, names):
     """
     Message handler from clients, takes a dictionary -
     a message from the clint, checks the correctness,
@@ -26,26 +26,67 @@ def process_client_message(message, messages_list, client):
     :param message:
     :param messages_list:
     :param client:
+    :param clients:
+    :param names:
     :return:
     """
     SERVER_LOGGER.debug(f'Parsing clients message: {message}')
-    # If success, receive and response
-    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
-            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
-        send_message(client, {RESPONSE: 200})
+    # If message
+    if ACTION in message and message[ACTION] == PRESENCE and \
+            TIME in message and USER in message:
+        # If user not registered -> register or send response and close connection
+        if message[USER][ACCOUNT_NAME] not in names.keys():
+            names[message[USER][ACCOUNT_NAME]] = client
+            send_message(client, RESPONSE_200)
+        else:
+            response = RESPONSE_400
+            response[ERROR] = 'User name is taken'
+            send_message(client, response)
+            clients.remove(client)
+            client.close()
         return
-    # If message add to line messages. No response.
-    elif ACTION in message and message[ACTION] == MESSAGE and TIME in message and MESSAGE_TEXT in message:
-        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
+    # If message -> add to queue
+    elif ACTION in message and message[ACTION] == MESSAGE and \
+            DESTINATION in message and TIME in message \
+            and SENDER in message and MESSAGE_TEXT in message:
+        messages_list.append(message)
         return
-    # Else Bad Request
+    # If client exit
+    elif ACTION in message and message[ACTION] == EXIT and ACCOUNT_NAME in message:
+        clients.remove(names[message[ACCOUNT_NAME]])
+        names[message[ACCOUNT_NAME]].close()
+        del names[message[ACCOUNT_NAME]]
+        return
+    # Else -> Bad request
     else:
-        send_message(client, {RESPONSE: 400, ERROR: 'Bad Request'})
+        response = RESPONSE_400
+        response[ERROR] = 'Bad response'
+        send_message(client, response)
         return
 
 
 @Log(SERVER_LOGGER)
-def create_arg_parser():
+def process_message(message, names, listen_socks):
+    """
+    The function send message to client. Received dictionary message, return nothing
+    :param message:
+    :param names:
+    :param listen_socks:
+    :return:
+    """
+    if message[DESTINATION] in names and names[message[DESTINATION]] in listen_socks:
+        send_message(names[message[DESTINATION]], message)
+        SERVER_LOGGER.info(f'Message has sent to {message[DESTINATION]} '
+                           f'from user {message[SENDER]}.')
+    elif message[DESTINATION] in names and names[message[DESTINATION]] not in listen_socks:
+        raise ConnectionError
+    else:
+        SERVER_LOGGER.error(
+            f'User {message[DESTINATION]} not registered.')
+
+
+@Log(SERVER_LOGGER)
+def arg_parser():
     """
     Command Line Argument Parser
     :return:
@@ -53,7 +94,16 @@ def create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
     parser.add_argument('-a', default='', nargs='?')
-    return parser
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+
+    # Port checking
+    if not 1023 < listen_port < 65536:
+        SERVER_LOGGER.critical(f'Try start with port: {listen_port}. Port range must be from 1024 to 65535.')
+        sys.exit(1)
+
+    return listen_address, listen_port
 
 
 def main():
@@ -63,15 +113,7 @@ def main():
     server.py -p 8888 -a 127.0.0.1
     :return:
     """
-    parser = create_arg_parser()
-    namespace = parser.parse_args(sys.argv[1:])
-    listen_address = namespace.a
-    listen_port = namespace.p
-
-    # Port checking
-    if not 1023 < listen_port < 65536:
-        SERVER_LOGGER.critical(f'Try start with port: {listen_port}. Port range must be from 1024 to 65535.')
-        sys.exit(1)
+    listen_address, listen_port = arg_parser()
     SERVER_LOGGER.info(f'Listen port: {listen_port}, Listen address: {listen_address}.')
 
     # Preparing socket
@@ -83,6 +125,9 @@ def main():
     # Clients list, message line
     clients = []
     messages = []
+
+    # Dictionary with users names and sockets
+    names = dict()  # {client_name: client_socket}
 
     # Port listening
     transport.listen(MAX_CONNECTIONS)
@@ -112,27 +157,21 @@ def main():
         if recv_data_lst:
             for client_with_message in recv_data_lst:
                 try:
-                    process_client_message(get_message(client_with_message), messages, client_with_message)
+                    process_client_message(get_message(client_with_message),
+                                           messages, client_with_message, clients, names)
                 except IncorrectDataReceivedError:
                     SERVER_LOGGER.info(f'Client {client_with_message.getpeername()} disconnect from server')
                     clients.remove(client_with_message)
 
-        # If messages to send and awaiting clients, send message
-        if messages and send_data_lst:
-            message = {
-                ACTION: MESSAGE,
-                SENDER: messages[0][0],
-                TIME: time.time(),
-                MESSAGE_TEXT: messages[0][1]
-            }
-            del messages[0]
-            for waiting_client in send_data_lst:
-                try:
-                    send_message(waiting_client, message)
-                except IncorrectDataReceivedError:
-                    SERVER_LOGGER.info(f'Client {waiting_client.getpeername()} disconnected from server.')
-                    waiting_client.close()
-                    clients.remove(waiting_client)
+        # If messages -> process every message
+        for i in messages:
+            try:
+                process_message(i, names, send_data_lst)
+            except Exception:
+                SERVER_LOGGER.info(f'Connect with client {i[DESTINATION]} has lost')
+                clients.remove(names[i[DESTINATION]])
+                del names[i[DESTINATION]]
+        messages.clear()
 
 
 if __name__ == '__main__':
